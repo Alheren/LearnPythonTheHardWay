ba64688f6d409a07de9f254ecc6fe1ea4b4e42c8
Sean Ivy <Sean Ivy>
2017-03-22 08:55:19 -0700

    Updated ex10 for bonus credit.

--- ex10.py
+++ ex10.py
@@ -2,3 +2,20 @@
 LPTHW - Exercise 10 - What Was That?
 
 """
+
+TABBY_CAT = "\tI'm tabbed in. %s"
+TABBY_CAT_2 = "And I have \n\tNew goodies."
+PERSIAN_CAT = "I'm split\non a line."
+BACKSLASH_CAT = "I'm \\ a \\ cat."
+
+FAT_CAT = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print TABBY_CAT % TABBY_CAT_2
+print PERSIAN_CAT
+print BACKSLASH_CAT
+print FAT_CAT

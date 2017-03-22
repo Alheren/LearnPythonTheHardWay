23df3734e03aafa937769450456b65f9afa816a6
Sean Ivy <Sean Ivy>
2017-03-21 19:05:56 -0700

    ex 7 complete

--- ex 7.py
+++ ex 7.py
@@ -3,3 +3,24 @@ LPTHW - Exercise 7 - More Printing
 
 """
 
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." *10 # what'd that do?
+
+END_01 = "C"
+END_02 = "h"
+END_03 = "e"
+END_04 = "e"
+END_05 = "s"
+END_06 = "e"
+END_07 = "B"
+END_08 = "u"
+END_09 = "r"
+END_10 = "g"
+END_11 = "e"
+END_12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+print END_01 + END_02 + END_03 + END_04 + END_05 + END_06,
+print END_07 + END_08 + END_09 + END_10 + END_11 + END_12

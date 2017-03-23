4e08d92de0ffaf7aaef912013478496da12df923
Alheren <sean@seanivy.com>
2017-03-23 12:58:15 -0700

    finished ex12

--- ex12.py
+++ ex12.py
@@ -2,3 +2,11 @@
 LPTHW - Exercise 12 - Prompting People
 
 """
+
+AGE = raw_input("How old are you? ")
+HEIGHT = raw_input("How tall are you? ")
+WEIGHT = raw_input("How much do you weight? ")
+
+print "So you are %s old, %s tall, and %s heavy." % (
+    AGE, HEIGHT, WEIGHT
+)
